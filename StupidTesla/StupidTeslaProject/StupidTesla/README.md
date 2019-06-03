15ab60a921b1ceb649e44361bdfd04e7cea1da14
Squiddle <thomas.passelegue@gmail.com>
2019-06-03 08:52:55 +0200

    Added StupidTesla

---
+++ StupidTesla/StupidTeslaProject/StupidTesla/README.md
@@ -0,0 +1,11 @@
+##Stupid Tesla
+
+Makes Unstable Tesla Coil stupid.
+	
+##Installation
+
+Drop `StupidTesla.dll` into `\BepInEx\plugins\`	
+
+## Changelog
+	- v1.0.0
+		- Release
---
+++ StupidTesla/StupidTeslaProject/StupidTesla/StupidTesla.cs
@@ -0,0 +1,42 @@
+﻿using BepInEx;
+using RoR2;
+using UnityEngine;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using System.Reflection;
+
+namespace StupidTesla
+{
+    [BepInDependency("com.bepis.r2api")]
+
+    [BepInPlugin("com.Squiddle.stupidtesla", "StupidTesla", "1.0.0")]
+
+    public class StupidTesla : BaseUnityPlugin
+    {
+
+        public float teslaRange = 500000f;
+
+        public void Awake()
+        {
+            //On.RoR2.Orbs.LightningOrb.Begin += (orig, self) =>
+            // {
+            //     if(self.lightningType == RoR2.Orbs.LightningOrb.LightningType.Tesla)
+            //     {
+            //         self.range = teslaRange;
+            //     }
+            //     orig(self);
+            // };
+
+            IL.RoR2.CharacterBody.UpdateTeslaCoil += (il) =>
+            {
+                var c = new ILCursor(il);
+                c.GotoNext(
+                    x => x.MatchLdcR4(35)
+                );
+                c.Remove();
+                c.Emit(OpCodes.Ldc_R4, teslaRange);
+            };
+        }
+        
+    }
+}
